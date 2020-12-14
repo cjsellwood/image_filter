c405dbe65fa5bc8516880e3acfb7d43bc4f47969
cjsellwood <c.sellwood22@gmail.com>
2020-12-14 18:03:50 +1100

    Edge highlighting started

---
+++ README.md
@@ -0,0 +1,2 @@
+# Image Filter
+Upload an image and apply filters to it. Can then be downloaded.
--- scripts.js
+++ scripts.js
@@ -46,30 +46,6 @@ function handleUpload(e) {
 
 imageForm.addEventListener("submit", handleUpload);
 
-// function changeColor(e) {
-//   console.log(e);
-
-//   // get rgba data from each pixel of image on canvas
-//   const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-//   console.log(pixels.data);
-
-//   // loop over each pixel
-//   for (let i = 0; i < pixels.data.length; i += 4) {
-//     // pixels.data[i] = pixels.data[i] + Math.floor(Math.random() * 10) - 5;
-//     // pixels.data[i + 1] = pixels.data[i + 1] + Math.floor(Math.random() * 10) - 5;
-//     // pixels.data[i + 2] = pixels.data[i + 2] + Math.floor(Math.random() * 10) - 5;
-//     // pixels.data[i + 3] = pixels.data[i + 3] + Math.floor(Math.random() * 10) - 5;
-//   }
-//   // console.log(rgbPixels);
-
-//   // Add to canvas
-//   ctx.putImageData(pixels, 0, 0);
-// }
-
-// canvas.addEventListener("click", changeColor);
-
-const filterForm = document.querySelector("#filter-form");
-
 let originalImage;
 
 // function to change image colors
@@ -103,193 +79,146 @@ let originalImage;
 //   ctx.drawImage(imageClone, 0, 0);
 // }
 
-const brightnessSlider = document.querySelector("#brightness-slider");
-brightnessSlider.addEventListener("input", (e) =>
-  colorChange("brightness", "%", e)
-);
-
-// // change grayscale amount
-// function grayscale() {
-//   // const pixels = new ImageData(
-//   //   new Uint8ClampedArray(originalImage.data),
-//   //   originalImage.width,
-//   //   originalImage.height
-//   // );
-
-//   // // loop over each pixel
-//   // for (let i = 0; i < pixels.data.length; i += 4) {
-//   //   const r = pixels.data[i + 0];
-//   //   const g = pixels.data[i + 1];
-//   //   const b = pixels.data[i + 2];
-//   //   const average = (r + g + b) / 3;
-//   //   pixels.data[i + 0] = average;
-//   //   pixels.data[i + 1] = average;
-//   //   pixels.data[i + 2] = average;
-//   // }
-
-//   // // Add to canvas
-//   // ctx.putImageData(pixels, 0, 0);
-
-//   console.log("grayscale");
-//   // use canvas filters
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   ctx.filter = `grayscale(${this.value}%)`;
-//   const imageClone = new Image();
-//   imageClone.src = drawnImage.src;
-//   ctx.drawImage(imageClone, 0, 0);
-// }
-
-const grayscaleSlider = document.querySelector("#grayscale-slider");
-grayscaleSlider.addEventListener("input", (e) =>
-  colorChange("grayscale", "%", e)
-);
-
-// function sepia() {
-//   // const pixels = new ImageData(
-//   //   new Uint8ClampedArray(originalImage.data),
-//   //   originalImage.width,
-//   //   originalImage.height
-//   // );
-
-//   // // loop over each pixel
-//   // for (let i = 0; i < pixels.data.length; i += 4) {
-//   //   const r = pixels.data[i + 0];
-//   //   const g = pixels.data[i + 1];
-//   //   const b = pixels.data[i + 2];
-//   //   pixels.data[i + 0] = 0.393 * r + 0.769 * g + 0.189 * b;
-//   //   pixels.data[i + 1] = 0.349 * r + 0.686 * g + 0.168 * b;
-//   //   pixels.data[i + 2] = 0.272 * r + 0.534 * g + 0.131 * b;
-//   // }
-
-//   // // Add to canvas
-//   // ctx.putImageData(pixels, 0, 0);
-
-//   console.log("sepia");
-//   // use canvas filters
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   ctx.filter = `sepia(${this.value}%)`;
-//   const imageClone = new Image();
-//   imageClone.src = drawnImage.src;
-//   ctx.drawImage(imageClone, 0, 0);
-// }
-
-const sepiaSlider = document.querySelector("#sepia-slider");
-sepiaSlider.addEventListener("input", (e) => colorChange("sepia", "%", e));
-
-// let inverted = false;
-
-// function invert() {
-//   // reset to default if
-//   // if (inverted) {
-//   //   resetImage();
-//   //   return;
-//   // }
-//   // const pixels = new ImageData(
-//   //   new Uint8ClampedArray(originalImage.data),
-//   //   originalImage.width,
-//   //   originalImage.height
-//   // );
-
-//   // // loop over each pixel
-//   // for (let i = 0; i < pixels.data.length; i += 4) {
-//   //   const r = pixels.data[i + 0];
-//   //   const g = pixels.data[i + 1];
-//   //   const b = pixels.data[i + 2];
-//   //   pixels.data[i + 0] = 255 - r;
-//   //   pixels.data[i + 1] = 255 - g;
-//   //   pixels.data[i + 2] = 255 - b;
-//   // }
-
-//   // inverted = true;
-
-//   // // Add to canvas
-//   // ctx.putImageData(pixels, 0, 0);
-
-//   console.log("invert");
-//   // use canvas filters
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   ctx.filter = `invert(${this.value}%)`;
-//   const imageClone = new Image();
-//   imageClone.src = drawnImage.src;
-//   ctx.drawImage(imageClone, 0, 0);
-// }
-
-const invertSlider = document.querySelector("#invert-slider");
-invertSlider.addEventListener("input", (e) => colorChange("invert", "%", e));
-
-// function blur() {
-//   console.log("blur");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   const imageClone = new Image();
-//   imageClone.src = drawnImage.src;
-//   ctx.filter = `blur(${this.value}px)`;
-//   ctx.drawImage(imageClone, 0, 0);
-// }
-
-const blurSlider = document.querySelector("#blur-slider");
-blurSlider.addEventListener("input", (e) => colorChange("blur", "px", e));
-
-// function saturation() {
-//   console.log("saturation");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   const imageClone = new Image();
-//   imageClone.src = drawnImage.src;
-//   ctx.filter = `saturate(${this.value}%)`;
-//   ctx.drawImage(imageClone, 0, 0);
-// }
-
-const saturationSlider = document.querySelector("#saturation-slider");
-saturationSlider.addEventListener("input", (e) =>
-  colorChange("saturate", "%", e)
-);
-
-// function hue() {
-//   console.log("hue");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   const imageClone = new Image();
-//   imageClone.src = drawnImage.src;
-//   ctx.filter = `hue-rotate(${this.value}deg)`;
-//   ctx.drawImage(imageClone, 0, 0);
-// }
-
-const hueSlider = document.querySelector("#hue-slider");
-hueSlider.addEventListener("input", (e) => colorChange("hue-rotate", "deg", e));
-
-// function opacity() {
-//   console.log("opacity");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   const imageClone = new Image();
-//   imageClone.src = drawnImage.src;
-//   ctx.filter = `opacity(${this.value}%)`;
-//   ctx.drawImage(imageClone, 0, 0);
-// }
-
-const opacitySlider = document.querySelector("#opacity-slider");
-opacitySlider.addEventListener("input", (e) => colorChange("opacity", "%", e));
-
-const contrastSlider = document.querySelector("#contrast-slider");
-contrastSlider.addEventListener("input", (e) =>
-  colorChange("contrast", "%", e)
-);
-
-function colorChange(property, unit, e) {
+function edges() {
+  // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // create new image as copy of original imag
   const imageClone = new Image();
   imageClone.src = drawnImage.src;
-  const newFilter = `${property}(${e.target.value}${unit})`;
-  filters[property] = newFilter;
+
+  // combine all filters
   const filtersArray = [];
   for (const filter in filters) {
-    filtersArray.push(filters[filter])
+    filtersArray.push(filters[filter]);
+  }
+
+  // apply filters
+  ctx.filter = filtersArray.join(" ");
+  ctx.drawImage(imageClone, 0, 0);
+  console.log("filter");
+
+  const filteredImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  console.log(filteredImage);
+
+  const pixels = new ImageData(
+    new Uint8ClampedArray(filteredImage.data),
+    filteredImage.width,
+    filteredImage.height
+  );
+
+  console.log(pixels);
+  const pixelsArray = [];
+
+  // create object for each pixel and add to array of pixels
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    // pixels.data[i + 0] = 255;
+    const one = {
+      r: pixels.data[i],
+      g: pixels.data[i + 1],
+      b: pixels.data[i + 2],
+      a: pixels.data[i + 3]
+    }
+    pixelsArray.push(one);
+  }
+
+  console.log(pixelsArray);
+  console.log(pixels.width);
+
+  const pixelGrid = [];
+
+  // create nested arrays as rows of pixels
+  for (let i = 0; i < pixels.height; i++) {
+    const inner = []
+    for (let j = 0; j < pixels.width; j++) {
+      inner.push(pixelsArray[i + j])
+    }
+    pixelGrid.push(inner);
+  }
+
+  console.log(pixelGrid[0]);
+
+  // test to see if can affect nested grid
+  for (let i = 450; i < 550; i++) {
+    for (let j = 450; j < 450; j++) {
+      pixelGrid[i][j].g = 255;
+    } 
+  }
+
+  const transformArray = [];
+  // transform back to image data object
+  for (let i = 0; i < pixelGrid.length; i++) {
+    for (let j = 0; j < pixelGrid[i].length; j++) {
+      transformArray.push(pixelGrid[i][j].r)
+      transformArray.push(pixelGrid[i][j].g)
+      transformArray.push(pixelGrid[i][j].b)
+      transformArray.push(pixelGrid[i][j].a)
+    }
+  }
+
+  console.log("here")
+  console.log(pixels.data)
+  console.log(transformArray)
+
+  const transformPixels = new ImageData(
+    new Uint8ClampedArray(transformArray),
+    filteredImage.width,
+    filteredImage.height
+  );
+
+  console.log(transformPixels);
+  ctx.putImageData(transformPixels, 0, 0);
+
+
+
+  // gx array and gy array
+  const gx = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
+  const gy = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]];
+
+  // copy pixels variable so that changes to image don't effect next calculation
+  const pixelsCopy = new ImageData(
+    new Uint8ClampedArray(filteredImage.data),
+    filteredImage.width,
+    filteredImage.height
+  );
+
+   // treat pixels outside boundaries as black(0,0,0)
+
+}
+
+const edgesButton = document.querySelector("#edges-button");
+edgesButton.addEventListener("click", edges);
+
+// Change image colours based on slider and its property
+function colorChange(property, unit, e) {
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // create new image as copy of original image
+  const imageClone = new Image();
+  imageClone.src = drawnImage.src;
+
+  // add new filter from slider value
+  const newFilter = `${property}(${e.target.value}${unit})`;
+  filters[property] = newFilter;
+
+  // combine all filters
+  const filtersArray = [];
+  for (const filter in filters) {
+    filtersArray.push(filters[filter]);
   }
   console.log(filtersArray);
+
+  // apply filters
   ctx.filter = filtersArray.join(" ");
   console.log(filtersArray.join(" "));
 
+  // draw image with filters too canvas
   ctx.drawImage(imageClone, 0, 0);
 }
 
-const filters = {
+// keep track of filters to apply
+let filters = {
   brightness: null,
   grayscale: null,
   sepia: null,
@@ -301,16 +230,78 @@ const filters = {
   contrast: null,
 };
 
+// Add event listener to each slider
+const brightnessSlider = document.querySelector("#brightness-slider");
+brightnessSlider.addEventListener("input", (e) =>
+  colorChange("brightness", "%", e)
+);
+
+const grayscaleSlider = document.querySelector("#grayscale-slider");
+grayscaleSlider.addEventListener("input", (e) =>
+  colorChange("grayscale", "%", e)
+);
+
+const sepiaSlider = document.querySelector("#sepia-slider");
+sepiaSlider.addEventListener("input", (e) => colorChange("sepia", "%", e));
+
+const invertSlider = document.querySelector("#invert-slider");
+invertSlider.addEventListener("input", (e) => colorChange("invert", "%", e));
+
+const blurSlider = document.querySelector("#blur-slider");
+blurSlider.addEventListener("input", (e) => colorChange("blur", "px", e));
+
+const saturationSlider = document.querySelector("#saturation-slider");
+saturationSlider.addEventListener("input", (e) =>
+  colorChange("saturate", "%", e)
+);
+
+const hueSlider = document.querySelector("#hue-slider");
+hueSlider.addEventListener("input", (e) => colorChange("hue-rotate", "deg", e));
+
+const opacitySlider = document.querySelector("#opacity-slider");
+opacitySlider.addEventListener("input", (e) => colorChange("opacity", "%", e));
+
+const contrastSlider = document.querySelector("#contrast-slider");
+contrastSlider.addEventListener("input", (e) =>
+  colorChange("contrast", "%", e)
+);
+
 // Restore original image and filters
-function resetImage(e) {
+function resetImage() {
+  // Replace with original image
   ctx.putImageData(originalImage, 0, 0);
+
+  // Reset sliders
   brightnessSlider.value = 0;
-  inverted = false;
+  grayscaleSlider.value = 0;
+  sepiaSlider.value = 0;
+  invertSlider.value = 0;
+  blurSlider.value = 0;
+  saturationSlider.value = 100;
+  hueSlider.value = 0;
+  opacitySlider.value = 100;
+  contrastSlider.value = 100;
+
+  // Reset filters
+  filters = {
+    brightness: null,
+    grayscale: null,
+    sepia: null,
+    invert: null,
+    blur: null,
+    saturate: null,
+    "hue-rotate": null,
+    opacity: null,
+    contrast: null,
+  };
+
+  console.log(filters);
 }
 
 const resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener("click", resetImage);
 
+// Download image
 function downloadImage() {
   const imageURL = canvas.toDataURL("image/png");
   console.log(imageURL);
